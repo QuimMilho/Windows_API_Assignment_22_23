408d7bd9cc3952054ab8ed260cd75e181dc4cb6b
Lucas Caetano <lucas.rib.caetano@gmail.com>
2023-05-01 11:56:20 +0100

    update

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,5 +1,58 @@
 #include <windows.h>
+#include <tchar.h>
+#include <fcntl.h>
+#include <io.h>
+#include <stdio.h>
 
-int wmain(int argc, TCHAR * argv[]) {
-	return 0;
+#define MAX 256
+
+int _tmain(int argc, LPTSTR argv[]) {
+
+#ifdef UNICODE 
+    if (_setmode(_fileno(stdin), _O_WTEXT) == -1) {
+        _tprintf(TEXT("Failed to set input mode.\n"));
+        return 1;
+    }
+    if (_setmode(_fileno(stdout), _O_WTEXT) == -1) {
+        _tprintf(TEXT("Failed to set output mode.\n"));
+        return 1;
+    }
+#endif
+
+    // Memória Partilhada
+
+    // Obtém a handle para o ficheiro
+    // CreateFile()
+
+
+    // Criar o objeto FileMapping usando o handle anterior
+    // HANDLE WINAPI CreateFileMapping (
+    //      HANDLE hFile, // Ficheiro a usar
+    //      LPSECURITY_ATTRIBUTES lpAttributes,
+    //      DWORD flProtect, // flags para: escrita/leitura/execução
+    //      DWORD dwMaximumSizeHigh, // Tamanho dado em duas DWORDS
+    //      DWORD dwMaximumSizeLow, // (mais significativo e menos significativo)
+    //      LPCTSTR lpName // Nome a dar ao recurso (fich. mapeado)
+    // );
+
+
+    // Mapear uma vista do ficheiro no seu espaço de endereçamento
+    // MapViewOfFile
+
+
+
+    // Usa a memória partilhada através da vista (sintaxe habitual ponteiros * -> [])
+
+
+
+    // Desmapeia a vista
+    // UnmapViewOfFile()
+
+    // Fecha a handle
+    // CloseHandle()
+
+    // Fecha a handle do ficheiro
+    // CloseHandle()
+
+    return 0;
 }
