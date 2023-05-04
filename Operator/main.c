0986b9d0f82cfc067c30e056c703c464ae90d787
Lucas Caetano <lucas.rib.caetano@gmail.com>
2023-05-04 16:56:02 +0100

    adicionado conteúdo da DLL ao operador

--- Operator/main.c
+++ Operator/main.c
@@ -10,6 +10,8 @@ int _tmain(int argc, TCHAR* argv[]) {
 
     char comString[50], com[30], arg1[10], arg2[10];
     int nArgs;
+    HANDLE WINAPI handle;
+    LPCTSTR pView;
 
 //-----------------------Unicode----------------------
 #ifdef UNICODE 
@@ -23,7 +25,95 @@ int _tmain(int argc, TCHAR* argv[]) {
     }
 #endif
 //-----------------------/Unicode---------------------
+
+    printf("|====================| Operador |====================|\n");
+
+    printf("Comandos Disponíveis (<> -> Obrigatório, [] -> Opcional):\n    stop <tempo em s>: Parar o movimento dos carros durante um determinado período de tempo (especificado em segundos).\n    obstaculo <linha>[coluna]: Inserir obstáculos nas faixas de rodagem que são intransponíveis pelos carros e pelos sapos.\n    inverte <linha>: Inverter o sentido da marcha de determinada faixa de rodagem.");
+
+    fflush(stdin);
+    fgets(comString, sizeof(comString), stdin);
+
+    //Usar função da DLL para abrir a memória partilhada e colocar lá a string
+
+
+    //------------------------------------------------------------------DLL------------------------------------------------------------------
+    /*
+        -> Abrir MP
+
+            HANDLE WINAPI handle;
+            LPCTSTR pView;
+
+            handle = OpenFileMapping(
+                FILE_MAP_READ,
+                FALSE,
+                "nome"
+            );
+
+            if (handle == NULL)
+            {
+                printf("Could not open file mapping object (%d).\n", GetLastError());
+                return 1;
+            }
+
+            pView = MapViewOfFile(
+                handle,
+                FILE_MAP_READ,
+                0,
+                0,                          //tenho ideia que o stor tinha dito para metermos 30 aqui not sure
+                104                         //Not sure do tamanho
+            );
+
+            if (pView == NULL)
+            {
+                printf("Could not map view of file (%d).\n", GetLastError());
+                CloseHandle(handle);
+                return 1;
+            }
+
+            UnmapViewOfFile(pView);
+
+            CloseHandle(handle);
     
+       -> Criar MP
+            
+            HANDLE WINAPI handle;
+            LPCTSTR pView;
+            
+            handle = CreateFileMapping(
+                INVALID_HANDLE_VALUE,       //  Não queremos nenhum ficheiro físico
+                NULL,
+                PAGE_READONLY,
+                0,
+                104,
+                NULL
+            );
+
+            if (handle == NULL)
+            {
+                printf("Could not create file mapping object (%d).\n", GetLastError());
+                return 1;
+            }
+
+            pView = MapViewOfFile(
+                handle,
+                FILE_MAP_READ,
+                0,
+                0,
+                0                           // Significa que começamos do início
+            );
+
+            if (pView == NULL)
+            {
+                printf("Could not map view of file (%d).\n", GetLastError());
+                CloseHandle(handle);
+                return 1;
+            }
+
+            UnmapViewOfFile(pView);
+
+            CloseHandle(handle);
+    */
+    //------------------------------------------------------------------DLL------------------------------------------------------------------
 
     //1 - Obtém handle para o objecto memória partilhada
     //
@@ -55,12 +145,7 @@ int _tmain(int argc, TCHAR* argv[]) {
     //5 – Fecha o handle
     //    CloseHandle()
 
-    printf("|====================| Operador |====================|\n");
-
-    printf("Comandos Disponíveis (<> -> Obrigatório, [] -> Opcional):\n    stop <tempo em s>: Parar o movimento dos carros durante um determinado período de tempo (especificado em segundos).\n    obstaculo <linha>[coluna]: Inserir obstáculos nas faixas de rodagem que são intransponíveis pelos carros e pelos sapos.\n    inverte <linha>: Inverter o sentido da marcha de determinada faixa de rodagem.");
-
-    fflush(stdin);
-
+    /* Tem de passar para o servidor    
     while (fgets(comString, sizeof(comString), stdin) == 0) {
 
         printf("\nComando Inválido... Comandos Disponíveis(<> -> Obrigatório, [] -> Opcional):\n    stop <tempo em s>: Parar o movimento dos carros durante um determinado período de tempo(especificado em segundos).\n    obstaculo <linha>[coluna]: Inserir obstáculos nas faixas de rodagem que são intransponíveis pelos carros e pelos sapos.\n    inverte <linha>: Inverter o sentido da marcha de determinada faixa de rodagem.\nInsira o comando: ");
@@ -74,6 +159,17 @@ int _tmain(int argc, TCHAR* argv[]) {
     printf("Com: %s", com);
     printf("arg1: %s", arg1);
     printf("arg2: %s", arg2);
+    */
+
+    // Criar o objeto FileMapping usando o handle anterior
+    //HANDLE WINAPI CreateFileMapping (
+    //     HANDLE hFile, // Ficheiro a usar
+    //     LPSECURITY_ATTRIBUTES lpAttributes,
+    //     DWORD flProtect, // flags para: escrita/leitura/execu��o
+    //     DWORD dwMaximumSizeHigh, // Tamanho dado em duas DWORDS
+    //     DWORD dwMaximumSizeLow, // (mais significativo e menos significativo)
+    //     LPCTSTR lpName // Nome a dar ao recurso (fich. mapeado)
+    //);
 
     return 0;
 }
