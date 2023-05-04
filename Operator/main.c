7075583b0806df466357840e59d94b8dd7d68060
Lucas Caetano <lucas.rib.caetano@gmail.com>
2023-05-04 12:57:17 +0100

    update comandos

--- Operator/main.c
+++ Operator/main.c
@@ -8,6 +8,10 @@
 
 int _tmain(int argc, TCHAR* argv[]) {
 
+    char comString[50], com[30], arg1[10], arg2[10];
+    int nArgs;
+
+//-----------------------Unicode----------------------
 #ifdef UNICODE 
     if (_setmode(_fileno(stdin), _O_WTEXT) == -1) {
         _tprintf(_T("Failed to set input mode.\n"));
@@ -18,8 +22,8 @@ int _tmain(int argc, TCHAR* argv[]) {
         return 1;
     }
 #endif
-
-
+//-----------------------/Unicode---------------------
+    
 
     //1 - Obtém handle para o objecto memória partilhada
     //
@@ -50,6 +54,26 @@ int _tmain(int argc, TCHAR* argv[]) {
 
     //5 – Fecha o handle
     //    CloseHandle()
+
+    printf("|====================| Operador |====================|\n");
+
+    printf("Comandos Disponíveis (<> -> Obrigatório, [] -> Opcional):\n    stop <tempo em s>: Parar o movimento dos carros durante um determinado período de tempo (especificado em segundos).\n    obstaculo <linha>[coluna]: Inserir obstáculos nas faixas de rodagem que são intransponíveis pelos carros e pelos sapos.\n    inverte <linha>: Inverter o sentido da marcha de determinada faixa de rodagem.");
+
+    fflush(stdin);
+
+    while (fgets(comString, sizeof(comString), stdin) == 0) {
+
+        printf("\nComando Inválido... Comandos Disponíveis(<> -> Obrigatório, [] -> Opcional):\n    stop <tempo em s>: Parar o movimento dos carros durante um determinado período de tempo(especificado em segundos).\n    obstaculo <linha>[coluna]: Inserir obstáculos nas faixas de rodagem que são intransponíveis pelos carros e pelos sapos.\n    inverte <linha>: Inverter o sentido da marcha de determinada faixa de rodagem.\nInsira o comando: ");
+        fflush(stdin);
+
+    }
+
+    nArgs = sscanf_s(comString, "%s %s %s", com, (unsigned)_countof(com), arg1, (unsigned)_countof(arg1), arg2, (unsigned)_countof(arg2));
+
+    printf("Nº Parâmetros: %d\n", nArgs);
+    printf("Com: %s", com);
+    printf("arg1: %s", arg1);
+    printf("arg2: %s", arg2);
 
     return 0;
 }
