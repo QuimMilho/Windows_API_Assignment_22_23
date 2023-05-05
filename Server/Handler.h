33bfb98c4938f9d2b3c0593f4e737392960eb97e
Lucas Caetano <lucas.rib.caetano@gmail.com>
2023-05-05 13:14:08 +0100

    registry update

--- Server/Handler.h
+++ Server/Handler.h
@@ -1,28 +1,17 @@
 #pragma once
 
+#include "Game.h"
+
 #include <Windows.h>
 
 // Thread Info
-
 typedef struct ThreadInfo { 
 	HANDLE thread;		// Handle da Thread
 	BOOL running;		// If thread must be running or not
 	DWORD threadId;		// Thread ID
 } THREADINFO;
 
-// Definições do jogo
-
-typedef struct GameSettings { 
-	DWORD lanes; 
-	DWORD init_speed; 
-} GAME_SETTINGS;
-
-// Load Settings
-
+// REGISTRY
+int createOptions(GAME_SETTINGS* gameSettings);
 int loadOptions(GAME_SETTINGS* gameSettings);
-
-int setOptions(GAME_SETTINGS* gameSettings, int k);
-
-// Game Thread
-
-DWORD WINAPI GameThread(LPVOID lpParam);
+int setOptions(HKEY hkey, GAME_SETTINGS* gameSettings, int k);
